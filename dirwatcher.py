a98b08861da2ce42a86f51a4943200530097bf01
Howard Post <hpost2012@gmail.com>
2020-06-28 21:40:10 -0700

    took out logging to file, it did both before this

--- dirwatcher.py
+++ dirwatcher.py
@@ -12,13 +12,13 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter(
     '%(asctime)s:%(funcName)s:%(levelname)s:%(message)s')
-file_handler = logging.FileHandler('dirwatcher.log')
-file_handler.setFormatter(formatter)
+# file_handler = logging.FileHandler('dirwatcher.log')
+# file_handler.setFormatter(formatter)
 stream_handler = logging.StreamHandler(sys.stdout)
 stream_handler.setLevel(logging.INFO)
 stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
-logger.addHandler(file_handler)
+# logger.addHandler(file_handler)
 
 # Global Variables
 exit_flag = False
